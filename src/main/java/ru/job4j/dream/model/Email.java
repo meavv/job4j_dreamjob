c8ab73825ec87b4cf785564e6aa91a3937ae40fb
meavv <c.c.c.@bk.ru>
2022-02-10 12:25:29 +0500

    4.1. JSON формат. [#238554]

---
+++ src/main/java/ru/job4j/dream/model/Email.java
@@ -0,0 +1,19 @@
+package ru.job4j.dream.model;
+
+public class Email {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Email{" + "name='" + name + '\'' + '}';
+    }
+}
--- src/main/java/ru/job4j/trash/Help.java
+++ src/main/java/ru/job4j/trash/Help.java
@@ -1,22 +1,7 @@
 package ru.job4j.trash;
 
-import java.util.Scanner;
 
 public class Help {
-    static int hour = 12;
-    static int min = 60;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int h = scanner.nextInt();
-        int m = scanner.nextInt();
-        Help help = new Help();
-        help.time(h, m);
-    }
 
-    public void time(int h, int m) {
-        int absHour = Math.abs(hour - h);
-        int absMin = Math.abs(min - m);
-        System.out.println((absHour >= 12 ? 0 : absHour) + " " + (absMin >= 60 ? 0 : absMin));
-    }
 }
